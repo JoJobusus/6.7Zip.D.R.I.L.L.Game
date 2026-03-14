c6bd86ee5ea8c2f5d076366368623bdead45863f
ZhenyaLaptev <vinditop909@gmail.com>
2026-03-14 10:15:33 +0200

    Fixed Adaptivity

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -2,17 +2,16 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-slate-900 border-b border-orange-500/30 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-2xl font-black tracking-widest text-orange-500">
-          <img src="/logo1.png" alt="" className="w-35 h-30"></img>
-        </Link>
-        <nav className="space-x-6">
-          <Link href="/" className="hover:text-orange-400 transition">Головна</Link>
-          <Link href="/games" className="hover:text-orange-400 transition">Ігри</Link>
-          <Link href="/team" className="hover:text-orange-400 transition">Команда</Link>
-        </nav>
-      </div>
+    <header className="flex items-center justify-between p-4">
+      <div className="logo font-bold">6.7Zip.D.R.I.L.L.Game</div>
+      <nav className="flex gap-2">
+        <button className="text-xs md:text-sm lg:text-base px-2 py-1">
+          Головна
+        </button>
+        <button className="text-xs md:text-sm lg:text-base px-2 py-1">
+          Про гру
+        </button>
+      </nav>
     </header>
   );
 }
